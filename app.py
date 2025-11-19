ca442343eb8889f1fd911d129618593b4430464e
aifrankaihenry-spec <aifrank.aihenry@gmail.com>
2025-11-19 21:45:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,8 @@ FRED_SERIES = {
     "sp500":            "SP500",        # S&P 500 :contentReference[oaicite:0]{index=0}
     "nasdaq":           "NASDAQCOM",    # Nasdaq Composite :contentReference[oaicite:1]{index=1}
     "dow":              "DJIA",         # Dow Jones Industrial Average :contentReference[oaicite:2]{index=2}
+    "russell2000": "RUTSL",
+
 }
 
 
@@ -401,5 +403,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
